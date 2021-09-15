f2d3f8b74ce86a9598561e5cceaa83b2b844ff3c
rufael negash <rufael.negash@gmail.com>
2021-09-15 12:42:33 +0200

    everything is diagonal

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :careerFrameworkData="careerFrameworkData" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import careerFrameworkData from "../data.json";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      careerFrameworkData
+    };
   }
-}
+};
 </script>
 
 <style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,46 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <!-- <h1>{{ skills }}</h1> -->
+    <table>
+      <thead>
+        <tr>
+          <td>-</td>
+          <th>novice</th>
+          <th>beginner</th>
+          <th>competent</th>
+          <th>proficient</th>
+          <th>expert</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="category in careerFrameworkData" :key="category.category">
+          <th
+            colspan="2"
+            :rowspan="category.skills.length"
+            :key="category.category"
+          >
+            {{ category.category }}
+          </th>
+          <table>
+            <tr v-for="skill in category.skills" :key="skill">
+              {{
+                skill.name
+              }}
+            </tr>
+          </table>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    careerFrameworkData: Array
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +58,10 @@ li {
 }
 a {
   color: #42b983;
+}
+
+table {
+  display: table;
+  width: 100%;
 }
 </style>
